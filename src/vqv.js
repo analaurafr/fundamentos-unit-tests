41a6a790669b7ded4e410fc7b9c176a34e92df74
analaurafr <laurafernandes@outlook.com>
2023-06-12 17:13:02 -0300

    trabalhando requisito 3 - melhorando formatação

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,10 @@
 
 function vqv(nome, idade) {
   if (nome && idade) {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+    return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   }
   return undefined;
 }
