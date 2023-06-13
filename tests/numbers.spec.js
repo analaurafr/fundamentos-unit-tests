b14627608e55d00d79ac00bbd6122dfe496c8575
analaurafr <laurafernandes@outlook.com>
2023-06-12 22:03:24 -0300

    trabalhando no requisitos numbers e productDetails

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,9 +1,10 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const numbers = require("../src/numbers");
+const numbers = require('../src/numbers');
 
-/*  A função `numbers` recebe um array de tamanho variável e retorna `true` se todos os parâmetros forem do tipo 'number' e `false` caso contrário.
+/*
+  A função `numbers` recebe um array de tamanho variável e retorna `true` se todos os parâmetros forem do tipo 'number' e `false` caso contrário.
 
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
@@ -13,15 +14,17 @@ const numbers = require("../src/numbers");
 
 */
 
-describe("Casos de testes para a função `numbers`", () => {
-  it("Deve retornar true quando o array contém apenas números", () => {
+describe('2 - Implemente os casos de teste para a função `numbers`', () => {
+  it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
+
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    expect(numbers([1, 2, 3, 4, 5])).toBe(true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    expect(numbers([1, 2, "3", 4, 5])).toBe(false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    expect(numbers([1, "a", 3])).toBe(false);
     // Escreva um teste em que a função recebe [' '] e retorna false
-    expect(numbers([" "])).toBe(false);
+
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true);
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false);
+    expect(numbers([1, 'a', 3])).toBe(false);
+    expect(numbers([' '])).toBe(false);
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,7 +31,7 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
     // Teste se o retorno da função é um array.
@@ -39,5 +39,13 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
+    expect(typeof productDetails).toBe('function');
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true);
+    expect(productDetails('Alcool gel', 'Máscara').length).toBe(2);
+    expect(typeof productDetails('Alcool gel', 'Máscara')[0]).toBe('object');
+    expect(typeof productDetails('Alcool gel', 'Máscara')[1]).toBe('object');
+    expect(productDetails('Alcool gel', 'Máscara')[0]).not.toBe(productDetails('Sorvete', 'Pipoca')[1]);
+    expect(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123')).toBe(true);
+    expect(productDetails('Alcool gel', 'Máscara')[1].details.productId.endsWith('123')).toBe(true);
   });
 });
